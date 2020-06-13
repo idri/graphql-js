eba5133ac77476336123ace1dfdba97c91bee6d1
GitHub Action Script <please@open.issue>
2020-06-14 01:15:05 +0300

    Deploy d100bda1e03a612985b2a15ba5756217c1960359 to 'npm' branch

---
+++ utilities/idriSortSchema.d.ts
@@ -0,0 +1,8 @@
+import { GraphQLSchema } from '../type/schema';
+
+/**
+ * Sort GraphQLSchema.
+ *
+ * This function returns a sorted copy of the given GraphQLSchema and a ordering similarto Intellij JS Graphql Plugin.
+ */
+export function idriSortSchema(schema: GraphQLSchema): GraphQLSchema;
